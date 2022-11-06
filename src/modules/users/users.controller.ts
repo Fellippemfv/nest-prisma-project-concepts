edc47addfe383bdbed54b7724b2322bc12d7a316
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2022-11-06 11:39:46 -0400

    Update

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,7 +13,8 @@ import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 
-import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
+import { ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+
 import { UserEntity } from "./entities/user.entity";
 
 @Controller("users")
@@ -22,31 +23,46 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
+    @ApiOperation({
+        summary: "Create a user",
+    })
     @ApiCreatedResponse({ type: UserEntity })
     async create(@Body() data: CreateUserDto) {
         return this.usersService.create(data);
     }
 
     @Get()
+    @ApiOperation({
+        summary: "Show all users",
+    })
     @ApiCreatedResponse({ type: UserEntity })
     async findAll() {
         return this.usersService.findAll();
     }
 
     @Get(":id")
+    @ApiOperation({
+        summary: "Show only one user by id",
+    })
     @ApiCreatedResponse({ type: UserEntity })
     async findOne(@Param("id") id: string) {
         return this.usersService.findOne(id);
     }
 
-    @ApiCreatedResponse({ type: UserEntity })
     @Put(":id")
+    @ApiOperation({
+        summary: "Update a user by id",
+    })
+    @ApiCreatedResponse({ type: UserEntity })
     async update(@Param("id") id: string, @Body() data: UpdateUserDto) {
         return this.usersService.update(id, data);
     }
 
-    @ApiCreatedResponse({ type: UserEntity })
     @Delete(":id")
+    @ApiOperation({
+        summary: "Delete a user by id",
+    })
+    @ApiCreatedResponse({ type: UserEntity })
     async remove(@Param("id") id: string) {
         return this.usersService.delete(id);
     }
